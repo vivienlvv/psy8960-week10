4d3a2f091ee225364b81d76c4bd6a141c00842d5
Vivien Lee <vivienlvv@gmail.com>
2023-03-31 15:39:27 -0500

    Modified data cleaning process to use a single pipe

--- R/week10.R
+++ R/week10.R
@@ -6,22 +6,15 @@ library(caret)
 
 # Data Import and Cleaning
 
+
 ## Dropping rows with missing value for hours worked across part time and 
 ## full time, NEED TO CONFIRM VARIABLE NAME
 gss_tbl = read_sav("../data/GSS2016.sav") %>% 
+  mutate(across(everything(), as.numeric)) %>% 
   drop_na(HRS1) %>% 
-  rename(workhours = HRS1)
-
-## 75% missingness flag 
-retain_flag = sapply(gss_tbl, function(x) sum(is.na(x))/nrow(gss_tbl)) < 0.75
-
-## Removing variables (columns) with less than 75% missingness 
-gss_tbl = gss_tbl[, retain_flag]
-
-## Getting rid of SPSS meta-data 
-gss_tbl = data.frame(lapply(gss_tbl, as.numeric))
-
-
+  # Double check missing value cleaning
+  rename(workhours = HRS1) %>% 
+  select_if(sapply(., FUN = function(x) sum(is.na(x))/nrow(.) < 0.75))
 
 
 # Visualization
